0596cfdbc46013f1bfcdc0ebcce9ad6145dcc525
Jagan <62688141+jagan510710@users.noreply.github.com>
2025-06-05 10:53:01 +0530

    Add files via upload
    Initial commit

---
+++ Sample/Sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sample
+//
+//  Created by m2019511 on 30/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
